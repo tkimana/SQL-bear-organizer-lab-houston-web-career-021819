5c2d08404039826d48570a39f43507d992c90cb1
Theodore Kimana <kimana.theodore@gmail.com>
2019-03-07 01:16:17 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
+(2,"Rowdy", 1, "M", "black", "intense", 2),
+(3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
+(4,"Sargeant Brown", 19, "M", "Green", "Slimy", 5),
+(5,"Melissa", 13, "F", "dark brown", "goofy", 4),
+(6,"Grinch", 2, "M", "Black", "Grinchy", 7),
+(7,"Wendy", 5, "F", "Blue", "naive", 1),
+(8,null, 3, "M", "black", "aggressive", 8);
--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,9 +1,9 @@
 def selects_all_female_bears_return_name_and_age
-  "Write your SQL query here"
+ SELECT * FROM ("Tabitha", 6, "F")
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "Write your SQL query here"
+  SELECT * FROM ("Grinch","Mr. Chocolate","Melissa","Rowdy","Sargeant Brown","Tabitha""Wendy",);
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
